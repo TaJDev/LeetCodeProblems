fe35ad590c41b9076843c4abe6bcf9c89cc307e2
T Jackson <77298367+TaJDev@users.noreply.github.com>
2023-10-18 10:58:10 -0400

    Modified Code w/ Comments

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -2,21 +2,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> output; 
-        int size = nums.size();
-        bool done = false;
+        // Variables
+        vector<int> output; // For Output Later
+        int size = nums.size(); // Needed for looping through vector
+        bool done = false; // Just a little boolean for breaking
         
-        for(int i = 0; i < size; i++) {
-            for(int e = 0; e < size; e++){
-                if(i == e) {continue;} 
-                if(nums[i] + nums[e] == target){
-                    output.push_back(i);
-                    output.push_back(e); 
-                    done = true; 
+        for(int i = 0; i < size; i++) { // Grab a number (will do all the nums)
+            for(int e = 0; e < size; e++){ // Compare grabbed number to every other number
+                if(i == e) {continue;} // If the index is the same, continue on (wont look at next if statement)
+                if(nums[i] + nums[e] == target){ // Brute force to check each number to see if it adds up to target
+                    output.push_back(i); // Store the index of the first number
+                    output.push_back(e); // Store the index of the second number
+                    done = true; // Save a variable that denotes the code as done
                 }
             }
-            if(done == true) {break;}
+            if(done == true) {break;} // Check to see if we found everything we need
         }
-        return output;
+        return output; // Return the computed output
     }
 };
