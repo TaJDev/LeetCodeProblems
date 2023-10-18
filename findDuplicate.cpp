f3e9a0887f09f48219ddcb36bf678448b6a3aef9
T Jackson <77298367+TaJDev@users.noreply.github.com>
2023-10-18 11:01:28 -0400

    Modified Code w/ Comments

--- findDuplicate.cpp
+++ findDuplicate.cpp
@@ -2,15 +2,16 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int output;
-        int size = nums.size();
+        // Variables
+        int output; // For Output Later
+        int size = nums.size(); // Needed for looping through vector
 
-        sort(nums.begin(),nums.end());
-        for(int i=0; i < size-1; i++) {
-            if(nums[i] == nums[i+1])
-                output = nums[i];
+        sort(nums.begin(),nums.end()); // Sort vector to easily find duplicates (Helps with bigger inputs)
+        for(int i=0; i < size-1; i++) { // Loop through all the items (But the last one since we will be accessing two items at a time
+            if(nums[i] == nums[i+1]) // If two items next to each other are the same
+                output = nums[i]; // We found the duplicate!
         }
         
-        return output;
+        return output; // Return the computed output
     }
 };
